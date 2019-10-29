0fddf5ec768ac8fd73488b5974b6c4db1fbc177a
Tanner Burns <tannerburns@Tanners-MacBook-Pro.local>
2019-10-29 10:24:04 -0500

    updated functions to be able to be used as standalone

--- vast/vast.py
+++ vast/vast.py
@@ -23,21 +23,21 @@ class Vast(object):
         else:
             return fn()
     
-    async def _get_future(self, fn: Callable, args: list= [], kwargs: dict= {}):
+    async def get_future(self, fn: Callable, args: list= [], kwargs: dict= {}):
         return self.loop.run_in_executor(self.executor, partial(self._execute, fn, args, kwargs))
     
-    async def _run_in_executor(self, fn: Callable, listOfArgs: List[Tuple[list, dict]]):
+    async def run_in_executor(self, fn: Callable, listOfArgs: List[Tuple[list, dict]]):
         with ThreadPoolExecutor(max_workers= self.workers) as self.executor:
             return [
                 await asyncio.gather(
-                    *[self._get_future(fn, *args) for args in listOfArgs[index:index+self.workers]]
+                    *[self.get_future(fn, *args) for args in listOfArgs[index:index+self.workers]]
                 )
                 for index in range(0, len(listOfArgs), self.workers)
             ]
     
     def run_in_eventloop(self, fn: Callable, listOfArgs: List[Tuple[list, dict]]= list((list, dict))):
         self.loop = asyncio.new_event_loop()
-        return [future.result() for future_results in self.loop.run_until_complete(self._run_in_executor(fn, listOfArgs)) for future in future_results]
+        return [future.result() for future_results in self.loop.run_until_complete(self.run_in_executor(fn, listOfArgs)) for future in future_results]
 
     
     
