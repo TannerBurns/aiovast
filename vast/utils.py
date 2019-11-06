b7e3696f245f54528a3dd8c872101478e997ee27
Tanner Burns <tannerburns@Tanners-MacBook-Pro.local>
2019-11-06 17:33:18 -0600

    updated method names for readability and added some comments

--- vast/utils.py
+++ vast/utils.py
@@ -30,29 +30,22 @@ class vast_fragment(object):
 
     __slots__ = 'fn', 'args', 'kwargs', '__dict__', '__weakref__'
 
-    def __new__(*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         if not args:
-            raise TypeError("init of vast_fragment needs an argument")
+            raise TypeError('init of vast_fragment needs an argument')
         if len(args) < 2:
-            raise TypeError("type 'vast_fragment' takes at least one argument")
-        basecls, fn, *args = args
-        if not callable(fn):
-            raise TypeError("the first argument must be callable")
-
-        self = super(vast_fragment, basecls).__new__(basecls)
-
-        self.fn = fn
-        self.args = args
+            raise TypeError('type "vast_fragment" takes at least one argument')
+        self.fn, *self.args = args
+        if not callable(self.fn):
+            raise TypeError('the first argument must be callable')
         self.kwargs = kwargs
-        return self
 
     def __call__(self, *args, **kwargs):
         kwargs.update(self.kwargs)
         return self.fn(*self.args, *args, **kwargs)
 
     def __reduce__(self):
-        return type(self), (self.fn,), (self.fn, self.args,
-               self.kwargs or None, self.__dict__ or None)
+        return type(self), (self.fn,), (self.fn, self.args, self.kwargs or None, self.__dict__ or None)
 
     def __setstate__(self, state):
         if not isinstance(state, tuple):
@@ -60,10 +53,8 @@ class vast_fragment(object):
         if len(state) != 4:
             raise TypeError(f'expected 4 items in state, got {len(state)}')
         self.fn, self.args, self.kwargs, namespace = state
-        if namespace:
-            self.__dict__ = namespace
-        else:
-            self.__dict__ = {}
+        self.__dict__ = namespace if namespace else {}
+            
         
 
         
